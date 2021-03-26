ce5111dcfbd6e7b20105319ebbe3bb884effda3d
Reményi Gergely <remenyig@gmail.com>
2021-03-26 14:52:51 +0100

    Add Prometheus Adapter chart + NodeJs Express chart + NodeJs container
    In order to make monitoring working, Prometheus Operator has to be installed via Helm: "helm repo add bitnami https://charts.bitnami.com/bitnami && helm install prometheus-operator bitnami/kube-prometheus"

---
+++ container/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+IMAGE="node-hpa-example"
+TAG="eu.gcr.io/trusty-server-307212/node-hpa-example"
+HELMCHART=$IMAGE
+SERVICE=$IMAGE
+PORT=8080
+
+echo "Building Docker Image: ${IMAGE}"
+docker build -t $IMAGE .
+
+echo "Tagging Docker Image ${IMAGE} with ${TAG}"
+docker tag  $IMAGE $TAG
+
+echo "Pushing Docker Image ${IMAGE} with ${TAG}"
+docker push $TAG
+
+echo "Updating helm chart ${HELMCHART}"
+helm install $HELMCHART ../$HELMCHART || helm uninstall $HELMCHART && helm install $HELMCHART ../$HELMCHART
+
+echo "Port forwarding kubernetes service ${SERVICE} to port ${PORT}"
+sleep 10 # Waiting for pod running state
+kubectl port-forward services/$SERVICE $PORT
---
+++ container/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const promMid = require('express-prometheus-middleware');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use(promMid({
+  metricsPath: '/metrics',
+  collectDefaultMetrics: true,
+  requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+  /**
+   * Uncomenting the `authenticate` callback will make the `metricsPath` route
+   * require authentication. This authentication callback can make a simple
+   * basic auth test, or even query a remote server to validate access.
+   * To access /metrics you could do:
+   * curl -X GET user:password@localhost:9091/metrics
+   */
+  // authenticate: req => req.headers.authorization === 'Basic dXNlcjpwYXNzd29yZA==',
+  /**
+   * Uncommenting the `extraMasks` config will use the list of regexes to
+   * reformat URL path names and replace the values found with a placeholder value
+  */
+  // extraMasks: [/..:..:..:..:..:../],
+  /**
+   * The prefix option will cause all metrics to have the given prefix.
+   * E.g.: `app_prefix_http_requests_total`
+   */
+  // prefix: 'app_prefix_',
+  /**
+   * Can add custom labels with customLabels and transformLabels options
+   */
+  // customLabels: ['contentType'],
+  // transformLabels(labels, req) {
+  //   // eslint-disable-next-line no-param-reassign
+  //   labels.contentType = req.headers['content-type'];
+  // },
+}));
+
+app.get("/getload", (req, res) => {
+  let x = 0.0001;
+  for (let i = 0; i < 1000000; i++){
+    x += Math.sqrt(x.toString());
+  }
+res.send("OK!");
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
